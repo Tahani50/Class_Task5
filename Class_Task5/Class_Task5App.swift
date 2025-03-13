5bcc7ba6d48974811d9daa673440d37e4f784d83
Tahani50 <tahanyayman4@gmail.com>
2025-03-13 12:33:13 +0300

    Initial Commit

---
+++ Class_Task5/Class_Task5App.swift
@@ -0,0 +1,17 @@
+//
+//  Class_Task5App.swift
+//  Class_Task5
+//
+//  Created by Taibah Valley Academy on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Class_Task5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
